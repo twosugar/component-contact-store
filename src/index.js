6221b6277a2de1fb46cd1686b78e2b3c6e7a7f34
wb-ty426248 <wb-ty426248@alibaba-inc.com>
2019-09-27 18:00:52 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 class Store {
   constructor() {
     this.state = {}
-    this.fnList = {}
+    this.events = {}
+  }
+
+  listen(key, fn) {
+    if (!key || !fn) {
+      return;
+    }
+    this.events[key] = this.events[key] || [];
+    if (this.events[key].indexOf(fn)) {
+      this.events[key].push(fn);
+    }
+  }
+
+  dispatch(key, value) {
+    const oldValue = this.state[key];
+    this.state[key] = value;
+    if (this.events[key] && this.events[key].length) {
+      for (const fn of this.events[key]) {
+        fn(oldValue, value);
+      }
+    }
   }
 }
 
-window.$com_store = new Store();
+window.$com_store = window.$com_store || new Store();
 module.exports = window.$com_store;
